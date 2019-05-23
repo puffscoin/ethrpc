c5544bbea5bdb8fda9c17a304cecff12eaafca8b
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-23 13:52:57 -0300

    Update env.js

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var ethrpc = global.ethrpc = require("./src");
+var puffsrpc = global.puffsrpc = require("./src");
 var logError = global.logError = require("./src/utils/log-error");
 
 var connectOptions = global.connectOptions = {
@@ -10,15 +10,15 @@ var connectOptions = global.connectOptions = {
   errorHandler: logError,
 };
 
-ethrpc.setDebugOptions({ connect: true });
+puffsrpc.setDebugOptions({ connect: true });
 
-ethrpc.connect(connectOptions, function (err) {
+puffsrpc.connect(connectOptions, function (err) {
   if (err) return console.error(err);
-  ethrpc.eth.coinbase(function (err, coinbase) {
+  puffsrpc.eth.coinbase(function (err, coinbase) {
     if (err) return console.error(err);
     if (coinbase == null) console.log("coinbase address not found");
   });
-  ethrpc.net.version(function (err, networkID) {
+  puffsrpc.net.version(function (err, networkID) {
     if (err) return console.error(err);
     if (networkID == null) console.error("net_version failed");
   });
