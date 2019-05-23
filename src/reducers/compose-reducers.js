8292c7533778509ab88a4dfb88a54f3a253d5a33
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-23 14:02:29 -0300

    Update compose-reducers.js

--- src/reducers/compose-reducers.js
+++ src/reducers/compose-reducers.js
@@ -4,11 +4,11 @@ var assign = require("lodash").assign;
 
 /**
  * @param {function} customReducer External (user-specified) reducer.
- * @param {function} reducer Default ethrpc reducer.
+ * @param {function} reducer Default puffsrpc reducer.
  */
 function composeReducers(customReducer, reducer) {
   return function (state, action) {
-    return assign({}, customReducer(state, action), { ethrpc: reducer(state.ethrpc, action) });
+    return assign({}, customReducer(state, action), { puffsrpc: reducer(state.puffsrpc, action) });
   };
 }
 
