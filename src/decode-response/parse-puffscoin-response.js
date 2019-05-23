d43b7751d9427034fd011ba1ddae508d2ac01b2a
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-23 13:58:35 -0300

    Update and rename parse-ethereum-response.js to parse-puffscoin-response.js

--- src/decode-response/parse-puffscoin-response.js
+++ src/decode-response/parse-puffscoin-response.js
@@ -3,10 +3,10 @@
 var isObject = require("../utils/is-object");
 var RPCError = require("../errors/rpc-error");
 
-function parseEthereumResponse(response, callback) {
+function parsePuffscoinResponse(response, callback) {
   if (!isObject(response)) return callback(new Error(JSON.stringify(response)));
   if (response.error != null) return callback(new RPCError(response.error));
   callback(null, response.result);
 }
 
-module.exports = parseEthereumResponse;
+module.exports = parsePuffscoinResponse;
