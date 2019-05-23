4874815eded57c1c5fbe4be7b9ecf1b8de7e2af5
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-23 14:18:14 -0300

    Update is-global-web3.js

--- src/utils/is-global-web3.js
+++ src/utils/is-global-web3.js
@@ -3,7 +3,7 @@
 function isGlobalWeb3() {
   if (typeof window === "undefined") return false;
   if (!window) return false;
-  if (!window.ethereum) {
+  if (!window.puffscoin) {
     if (window.web3 && window.web3.currentProvider) return true;
     return false;
   }
