43b3c44f2224f4ed5f41952dfebc8887059517d7
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-23 15:57:55 -0300

    Update helpers.js

--- test/helpers.js
+++ test/helpers.js
@@ -2,7 +2,7 @@
 
 var assert = require("chai").assert;
 var os = require("os");
-var StubServer = require("ethereumjs-stub-rpc-server");
+var StubServer = require("puffscoinjs-stub-rpc-server");
 var rpc = require("../src");
 
 function errorHandler(err) {
@@ -10,15 +10,15 @@ function errorHandler(err) {
 }
 
 module.exports.getIpcAddress = function () {
-  return process.env.ETHRPC_TEST_IPC_ADDRESS || ((os.type() === "Windows_NT") ? "\\\\.\\pipe\\TestRPC" : "testrpc.ipc");
+  return process.env.PUFFSRPC_TEST_IPC_ADDRESS || ((os.type() === "Windows_NT") ? "\\\\.\\pipe\\TestRPC" : "testrpc.ipc");
 };
 
 module.exports.getWsAddress = function () {
-  return process.env.ETHRPC_TEST_WS_ADDRESS || "ws://localhost:1337";
+  return process.env.PUFFSRPC_TEST_WS_ADDRESS || "ws://localhost:1337";
 };
 
 module.exports.getHttpAddress = function () {
-  return process.env.ETHRPC_TEST_HTTP_ADDRESS || "http://localhost:1337";
+  return process.env.PUFFSRPC_TEST_HTTP_ADDRESS || "http://localhost:1337";
 };
 
 module.exports.rpcConnect = function (transportType, transportAddress, callback) {
