9805a80859bd1ddd6bfb7df091d52ffb41d21471
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-08 13:23:17 -0300

    Update publish.js

--- src/wrappers/publish.js
+++ src/wrappers/publish.js
@@ -1,15 +1,15 @@
 "use strict";
 
-var eth = require("./eth");
+var puffs = require("./puffs");
 var RPCError = require("../errors/rpc-error");
 
 // publish a new contract to the blockchain from the coinbase account
 function publish(compiled, callback) {
   return function (dispatch) {
-    dispatch(eth.coinbase(function (err, coinbase) {
+    dispatch(puffs.coinbase(function (err, coinbase) {
       if (err) return callback(err);
       if (coinbase == null) return callback(new RPCError("COINBASE_NOT_SET"));
-      dispatch(eth.sendTransaction({ from: coinbase, data: compiled, gas: "0x6230b8" }, callback));
+      dispatch(puffs.sendTransaction({ from: coinbase, data: compiled, gas: "0x6230b8" }, callback));
     }));
   };
 }
