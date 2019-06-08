aebc66ec70baebe592c8bc2a8088b17b473a8fda
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-08 13:13:22 -0300

    Update tx-notify.js

--- src/transact/tx-notify.js
+++ src/transact/tx-notify.js
@@ -1,10 +1,10 @@
 "use strict";
 
-var eth_getTransaction = require("../wrappers/eth").getTransaction;
+var puffs_getTransaction = require("../wrappers/puffs").getTransaction;
 
 function txNotify(txHash, callback) {
   return function (dispatch, getState) {
-    dispatch(eth_getTransaction(txHash, function (err, transaction) {
+    dispatch(puffs_getTransaction(txHash, function (err, transaction) {
       if (err) return callback(err);
       if (transaction) return callback(null, transaction);
       dispatch({ type: "DECREMENT_HIGHEST_NONCE" });
