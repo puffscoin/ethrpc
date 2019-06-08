250a51083db05503f0784f79eea0cc276b5b0b14
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-08 12:56:17 -0300

    Update create-block-and-log-streamer.js

--- src/block-management/create-block-and-log-streamer.js
+++ src/block-management/create-block-and-log-streamer.js
@@ -2,21 +2,21 @@
 
 var Promise = require("es6-promise").Promise;
 var BlockAndLogStreamer = require("puffscoinjs-blockstream").BlockAndLogStreamer;
-var eth = require("../wrappers/eth");
+var puffs = require("../wrappers/puffs");
 var internalState = require("../internal-state");
 
 function createBlockAndLogStreamer() {
   return function (dispatch, getState) {
     internalState.set("blockAndLogStreamer", new BlockAndLogStreamer(function (hash) {
       return new Promise(function (resolve, reject) {
-        dispatch(eth.getBlockByHash([hash, false], function (err, block) {
+        dispatch(puffs.getBlockByHash([hash, false], function (err, block) {
           if (err) return reject(err);
           resolve(block);
         }));
       });
     }, function (filterOptions) {
       return new Promise(function (resolve, reject) {
-        dispatch(eth.getLogs(filterOptions, function (err, logs) {
+        dispatch(puffs.getLogs(filterOptions, function (err, logs) {
           if (err) return reject(err);
           if (logs == null) reject(new Error("Received null/undefined logs and no error."));
           resolve(logs);
