421e3c2eb06e13102ddbf75ede4096c64439e265
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-08 13:22:46 -0300

    Update is-unlocked.js

--- src/wrappers/is-unlocked.js
+++ src/wrappers/is-unlocked.js
@@ -1,15 +1,15 @@
 "use strict";
 
-var eth_sign = require("../wrappers/eth").sign;
+var puffs_sign = require("../wrappers/puffs").sign;
 
 /**
  * Check to see if the provided account is unlocked for the connected node.
  */
 function isUnlocked(account, callback) {
   return function (dispatch) {
-    dispatch(eth_sign([account, "0x00000000000000000000000000000000000000000000000000000000000f69b5"], function (err) {
+    dispatch(puffs_sign([account, "0x00000000000000000000000000000000000000000000000000000000000f69b5"], function (err) {
       if (err) {
-        console.warn("eth_sign failed during puffsrpc.isUnlocked:", err);
+        console.warn("puffs_sign failed during puffsrpc.isUnlocked:", err);
         return callback(null, false);
       }
       callback(null, true);
