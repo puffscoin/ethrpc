80482a67534c46a5363b015cc0c887415458edd5
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-08 13:02:54 -0300

    Update get-estimated-gas-with-buffer.js

--- src/encode-request/get-estimated-gas-with-buffer.js
+++ src/encode-request/get-estimated-gas-with-buffer.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var eth_estimateGas = require("../wrappers/eth").estimateGas;
+var puffs_estimateGas = require("../wrappers/puffs").estimateGas;
 var calculateEstimatedGasWithBuffer = require("./calculate-estimated-gas-with-buffer");
 
 function getEstimatedGasWithBuffer(packaged, callback) {
   return function (dispatch) {
-    dispatch(eth_estimateGas(packaged, function (err, estimatedGas) {
+    dispatch(puffs_estimateGas(packaged, function (err, estimatedGas) {
       if (err) return callback(err);
       callback(null, calculateEstimatedGasWithBuffer(estimatedGas));
     }));
