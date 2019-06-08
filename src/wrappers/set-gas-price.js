c1067bad2c39bc7afa6105b21cadc66f94de3229
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-08 13:24:42 -0300

    Update set-gas-price.js

--- src/wrappers/set-gas-price.js
+++ src/wrappers/set-gas-price.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var eth_gasPrice = require("./eth").gasPrice;
+var puffs_gasPrice = require("./puffs").gasPrice;
 var isHex = require("../utils/is-hex");
 
 function setGasPrice(callback) {
   return function (dispatch) {
-    dispatch(eth_gasPrice(null, function (err, gasPrice) {
+    dispatch(puffs_gasPrice(null, function (err, gasPrice) {
       if (err) return callback(err);
       if (gasPrice != null && isHex(gasPrice)) {
         dispatch({ type: "SET_GAS_PRICE", gasPrice: parseInt(gasPrice, 16) });
